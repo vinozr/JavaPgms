44a6a538a560081378d1e163116f069635ca19ee
vinozr <r.vinoz2k@gmail.com>
2019-12-30 20:23:02 +0530

    new java program

---
+++ src/com/learning/java/JavaPgm1.java
@@ -0,0 +1,12 @@
+package com.learning.java;
+
+public class JavaPgm1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Welcome to Java");
+
+	}
+
+}
